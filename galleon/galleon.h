001e7d675c2be36eb69e2d0b72c4607e2954548c
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-21 17:54:18 +0900

    ..

---
+++ galleon/galleon.h
@@ -0,0 +1,5 @@
+#ifndef _H_GALLEON
+#define _H_GALLEON
+
+#define MAX_COST 27
+#endif
--- galleon/main.c
+++ galleon/main.c
@@ -4,12 +4,13 @@
 
 int main(int argv, char** argc) {
   /*
-  Galleon* galleon = initGalleon(newGalleon());
+  Galleon* galleon = initGalleon(newGalleon(), MAX_COST);
 
   GalleonDeck *eastDeck = galleon_getDeckEast();
   GalleonDeck *westDeck = galleon_getDeckWest();
 
-  galleonDeck_setValue(eastDeck, newGalleonCard(..));
+
+  galleonDeck_setValue(eastDeck, 0, 0, newGalleonCard(..));
   galleon_start(galleon);
 
 
